0260673dd189b1c621ffcf7a0975936fd78e3e37
gopals355 <128239441+gopals355@users.noreply.github.com>
2023-03-18 14:27:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,24 @@
 import random
 
-class RPSAI:
+class RPS_AI:
     def __init__(self):
-        self.moves = ["rock", "paper", "scissors"]
-        self.winning_moves = {
-            "rock": "paper",
-            "paper": "scissors",
-            "scissors": "rock"
+        self.moves = ['rock', 'paper', 'scissors']
+        self.win_table = {
+            'rock': {'rock': 'tie', 'paper': 'lose', 'scissors': 'win'},
+            'paper': {'rock': 'win', 'paper': 'tie', 'scissors': 'lose'},
+            'scissors': {'rock': 'lose', 'paper': 'win', 'scissors': 'tie'}
         }
         self.results = []
 
     def record_result(self, player_move, ai_move):
-        if player_move == ai_move:
-            self.results.append("tie")
-        elif ai_move == self.winning_moves[player_move]:
-            self.results.append("loss")
-        else:
-            self.results.append("win")
+        self.results.append(player_move)
+        self.results.append(ai_move)
 
     def make_move(self):
-        if not self.results:
-            # If no results have been recorded yet, choose a random move
+        if len(self.results) < 2:
             return random.choice(self.moves)
-
-        last_result = self.results[-1]
-        if last_result == "win":
-            # If the last result was a win, choose the move that beats the player's last move
-            player_last_move = self.moves[self.moves.index(self.results[-2])]
-            return self.winning_moves[player_last_move]
-        elif last_result == "loss":
-            # If the last result was a loss, choose a random move
-            return random.choice(self.moves)
-        else:
-            # If the last result was a tie, choose the move that beats the player's last move
-            player_last_move = self.moves[self.moves.index(self.results[-2])]
-            return self.winning_moves[player_last_move]
-
-# Example usage:
-ai = RPSAI()
-while True:
-    player_move = input("Enter your move (rock/paper/scissors): ")
-    ai_move = ai.make_move()
-    print(f"AI move: {ai_move}")
-    ai.record_result(player_move, ai_move)
-    print(f"Results so far: {ai.results}\n")
+        player_last_move = self.moves[self.moves.index(self.results[-2])]
+        for move, result in self.win_table[player_last_move].items():
+            if result == 'win':
+                return move
+        return random.choice(self.moves)
